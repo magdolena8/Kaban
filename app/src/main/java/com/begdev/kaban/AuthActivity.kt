b049617076ebad33d537d539313808649fd115db
Ivan Beganski <ivanbegan16@gmail.com>
2022-12-01 12:00:49 +0300

    start

---
+++ app/src/main/java/com/begdev/kaban/AuthActivity.kt
@@ -0,0 +1,174 @@
+package com.begdev.kaban
+
+import android.content.Intent
+import android.content.IntentSender
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var btnSignInGoogle: SignInButton
+    private lateinit var btnRegister: Button
+
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var signUpRequest: BeginSignInRequest
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+
+    private companion object {
+        private const val TAG = "AuthActivity"
+        private const val RC_GOOGLE_SIGN_IN = 2
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+        auth = Firebase.auth;
+        currentUser = auth.currentUser!!
+
+        if(currentUser != null){
+            Log.d(TAG, "User is already logged ${currentUser.email}")
+            updateUI(currentUser)
+        }
+        else Log.d(TAG, "User is NULL $currentUser")
+
+
+
+        btnSignInGoogle = findViewById(R.id.btnSignInGoogle)
+        btnRegister = findViewById(R.id.btnRegister)
+        oneTapClient = Identity.getSignInClient(this)
+
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(
+                BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true)
+                    .build()
+            )
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            .setAutoSelectEnabled(true)    //may be true
+            .build()
+
+//        signUpRequest = BeginSignInRequest.builder()
+//            .setGoogleIdTokenRequestOptions(
+//                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+//                    .setSupported(true)
+//                    // Your server's client ID, not your Android client ID.
+//                    .setServerClientId(getString(R.string.default_web_client_id))
+//                    // Show all accounts on the device.
+//                    .setFilterByAuthorizedAccounts(false)
+//                    .build()
+//            )
+//            .build()
+
+        btnSignInGoogle.setOnClickListener {
+            oneTapClient.beginSignIn(signInRequest)
+                .addOnSuccessListener(this) { result ->
+                    try {
+                        startIntentSenderForResult(
+                            result.pendingIntent.intentSender, RC_GOOGLE_SIGN_IN,
+                            null, 0, 0, 0, null
+                        )
+                    } catch (e: IntentSender.SendIntentException) {
+                        Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    }
+                }
+                .addOnFailureListener(this) { e ->
+                    // No saved credentials found. Launch the One Tap sign-up flow, or
+                    // do nothing and continue presenting the signed-out UI.
+                    Log.d(TAG, "Log In error ${e.localizedMessage}")
+                }
+        }
+//        btnRegister.setOnClickListener{
+//            oneTapClient.beginSignIn(signUpRequest).addOnSuccessListener {
+//                try {
+//                    Toast.makeText(this, "Зарегались", Toast.LENGTH_LONG).show()
+////                    Log.d(TAG, )
+//
+//                } catch (e: IntentSender.SendIntentException) {
+//                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+//                }
+//            }.addOnFailureListener{
+//                Toast.makeText(this, "Ошибка регистрации", Toast.LENGTH_LONG).show()
+//
+//            }
+//        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        Toast.makeText(
+            baseContext, "UI updated -> Authenticated -> ${user?.email}",
+            Toast.LENGTH_SHORT
+        ).show()
+        val intentMainActivity: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intentMainActivity.apply {
+            putExtra("user", currentUser)
+        })
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            RC_GOOGLE_SIGN_IN -> {
+                try {
+                    val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken = googleCredential.googleIdToken
+                    when {
+                        idToken != null -> {
+                            // Got an ID token from Google. Use it to authenticate
+                            // with Firebase.
+                            val firebaseCredential =
+                                GoogleAuthProvider.getCredential(idToken, null)
+                            auth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this) { task ->
+                                    if (task.isSuccessful) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d(TAG, "signInWithCredential:success")
+                                        val user = auth.currentUser
+                                        updateUI(user)
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w(TAG,"signInWithCredential:failure",task.exception)
+//                                        updateUI(null)        //redraw or smth
+                                    }
+                                }
+                        }
+                        else -> {
+                            // Shouldn't happen.
+                            Log.d(TAG, "No ID token!")
+                        }
+                    }
+                    Log.d(TAG, "Got ID token -> $idToken")
+
+                } catch (e: ApiException) {
+                    Log.d(TAG, e.localizedMessage)
+                }
+            }
+        }
+    }
+
+}
